f9c572a5f48e37ef4daae5eb562ebd9d14473ebb
Daniel Cortes <d5t5c5@yahoo.com>
2018-09-14 18:24:47 -0500

    matched to mr mead

--- src/components/expensesSummary.js
+++ src/components/expensesSummary.js
@@ -1,20 +1,26 @@
 import React from 'react'
-import ExpenseTotal from '../selectors/expenses-total'
 import numeral from 'numeral'
 import { connect } from 'react-redux'
+import selectExpenses from '../selectors/expenses'
+import selectExpensesTotal from '../selectors/expenses-total'
 
-const ExpenseSummary = (props) => {
-    const expenseCount = props.expenses.length
-    const expenseTotal = ExpenseTotal(props.expenses)
+export const ExpenseSummary = ({ expenseCount, expensesTotal }) => {
+    const expenseWord = expenseCount === 1 ? 'expense' : 'expenses'
+    const formattedExpensesTotal = numeral(expensesTotal /100).format('$0,0.00')
+
         return (
-            <p>{(expenseCount ===1 ) ? `Viewing 1 expense totalling ${numeral(expenseTotal /100).format('$0,0.00')}` : 
-        `Viewing ${expenseCount} expenses totalling ${numeral(expenseTotal /100).format('$0,0.00')}`}</p>
+            <div>
+                <h1>Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}</h1>
+            </div>
         )
 }
 
 const mapStateToProps = (state) => {
+    const visibleExpenses = selectExpenses(state.expenses, state.filters)
+
     return {
-        expenses: state.expenses
+        expenseCount: visibleExpenses.length,
+        expensesTotal: selectExpensesTotal(visibleExpenses)
     }
 }
 
